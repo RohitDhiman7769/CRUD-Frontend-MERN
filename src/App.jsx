366de804b3f21a9ce1f3489ad390a38e7b906ae8
rohit <rohitDhiman@gmail.com>
2025-05-17 16:55:11 +0530

    Handle route

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,21 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import {  Routes, Route } from 'react-router-dom'
 import ViewDetails from './components/ViewUserDetails/ViewUserDetails'
 import UserDetails from './components/UserAddDetails/UserAddDetails'
 import { createContext, useContext, useEffect, useState } from 'react';
 
-function AppLayout() {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
-}
-
 export const context = createContext()
 
 const App = () => {
-  const [selectedUser, setSelectedUser] = useState(() => {
-    const stored = localStorage.getItem("user");
-    return stored ? JSON.parse(stored) : {};
-  });
-
-  // Save to localStorage when it changes
-  useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(selectedUser));
-    setSelectedUser(selectedUser)
-
-  }, [selectedUser]);
-
+  const [selectedUser, setSelectedUser] = useState();
   const contextValue = { selectedUser, setSelectedUser }
-  console.log('contextValue : ',contextValue)
-  const route = createBrowserRouter([
-    {
-      path: '/',
-      element: <context.Provider value={contextValue}><AppLayout /></context.Provider>,
-      children: [
-        {
-          path: '',
-          element: <UserDetails />
-        }, {
-          path: '/view',
-          element: <ViewDetails />
-        }
-      ]
-    }
-  ]);
 
   return (
-    <RouterProvider router={route} />
+    <context.Provider value={contextValue}>
+      <Routes>
+        <Route path="/" element={<UserDetails />}></Route>
+        <Route path="/view" element={<ViewDetails />}></Route>
+      </Routes>
+    </context.Provider>
   );
 };
-
 export default App;
